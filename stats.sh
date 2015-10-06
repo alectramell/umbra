44d3a536aebf6d56b9d3be1b201966656e4e4319
alectramell <alectramell@gmail.com>
2015-10-06 15:13:04 -0700

    UMBRA

--- stats.sh
+++ stats.sh
@@ -3,17 +3,15 @@
 clear
 
 USERNAME=$(whoami)
-HLOG=$(history)
+
+TODAY=$(date "+%m/%d/%Y")
 
 clear
 
-date "+%m/%d/%Y" > /home/$USERNAME/Desktop/stats.txt
-date "+%T" >> /home/$USERNAME/Desktop/stats.txt
-echo "$USERNAME" >> /home/$USERNAME/Desktop/stats.txt
-echo "$HLOG" >> /home/$USERNAME/Desktop/stats.txt
+history > /home/$USERNAME/Desktop/stats.txt
 
 clear
 
-zenity --text-info --title="Umbra | Stats" --font="ubuntu" --filename="/home/$USERNAME/Desktop/stats.txt" && rm -r /home/$USERNAME/Desktop/stats.txt
+zenity --text-info --title="Umbra | Stats for $TODAY" --font="Ubuntu" --filename="/home/$USERNAME/Desktop/stats.txt" && rm -r /home/$USERNAME/Desktop/stats.txt
 
 clear
