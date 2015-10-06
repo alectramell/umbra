e7e22f448aed8b53410d496380beaf31b7f027ad
alectramell <alectramell@gmail.com>
2015-10-06 15:28:17 -0700

    UMBRA

--- stats.sh
+++ stats.sh
@@ -8,10 +8,8 @@ TODAY=$(date "+%m/%d/%Y")
 
 clear
 
-sh /home/$USERNAME/umbra/brain.sh
+echo "Umbra v1.0 | Terminal stats for $TODAY"
 
-clear
+history
 
-zenity --text-info --title="Umbra | Stats for $TODAY" --font="Ubuntu" --filename="/home/$USERNAME/Desktop/stats.txt" && rm -r /home/$USERNAME/Desktop/stats.txt
-
-clear
+sleep 3
